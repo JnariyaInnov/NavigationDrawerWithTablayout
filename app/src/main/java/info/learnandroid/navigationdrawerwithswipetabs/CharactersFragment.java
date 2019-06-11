9bd6a47698fec3a5336e60ff21c48aa2d2764161
Sajal Gupta <sajalgupta762@gmail.com>
2019-06-11 15:09:50 +0530

    Initial commit

---
+++ app/src/main/java/info/learnandroid/navigationdrawerwithswipetabs/CharactersFragment.java
@@ -0,0 +1,10 @@
+package info.learnandroid.navigationdrawerwithswipetabs;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ratan on 10/17/2017.
+ */
+
+public class CharactersFragment extends Fragment {
+}
